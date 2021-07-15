e6425f23d0217d7d2eaab5183a78f01b60a10cc1
Giu Zambot <giuzambot@gmail.com>
2021-07-15 18:03:09 -0300

    Aqruivos para Heroku

--- transferir.bat
+++ transferir.bat
@@ -1,2 +1,2 @@
-robocopy . D:\reposit\desafio-frontend-modulo1-cubos /e /purge  /xd .git /xd .vscode /xf .gitignore
+robocopy . D:\reposit\desafio-frontend-modulo1-cubos /e /purge  /xd .git /xd .vscode
 pause
